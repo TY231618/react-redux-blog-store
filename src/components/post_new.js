39e291fabefdeb66f3717306abbc41dbd6f41c6e
Tony <anthony.c.young18@gmail.com>
2018-03-19 12:13:17 +0000

    added Link button to go to PostNew page

---
+++ src/components/post_new.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PostNew extends React.Component {
+
+  render() {
+    return (
+      <div>Post New</div>
+    )
+  }
+}
+
+export default PostNew;
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends React.Component {
 
@@ -25,6 +26,9 @@ class PostsIndex extends React.Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to='/posts/new'>New Post</Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           { this.renderPosts() }
