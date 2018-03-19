c39a92e79d2d84da2738608b1386d6c2df59352d
Tony <anthony.c.young18@gmail.com>
2018-03-19 16:11:26 +0000

    rendering single post to page

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions'; 
 
 class PostShow extends React.Component {
 
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.fetchPost(id);
+  }
+
   render() {
+
+    if(!this.props.post) {
+      return <div>LOADING...</div>
+    }
+    
     return (
-      <div>Post Show Page</div>
+      <div>
+        <h3>{ this.props.post.title }</h3>
+        <h6>{ this.props.post.categories }</h6>
+        <p>{ this.props.post.content }</p>
+      </div>
     )
   }
 }
 
-export default PostShow;
+function mapStateToProps(state, ownProps) {
+
+  return {
+    post: state.posts[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, { fetchPost: fetchPost })(PostShow);
