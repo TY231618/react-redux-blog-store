c024ff7ecb4f5157388ca1ea82708fbb63d3c708
Tony <anthony.c.young18@gmail.com>
2018-03-19 13:55:20 +0000

    added formReducer and created basic form with fields

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends React.Component {
 
+  renderField(field) {
+
+    return (
+      <div className="form-group">
+        <label>{ field.label }</label>
+        <input 
+          className="form-control"
+          type="text"
+          { ...field.input }
+        />
+      </div>
+    )
+
+  }
+
   render() {
     return (
-      <div>Post New</div>
+      <form>
+        <Field 
+          label="Title"
+          name="title"
+          component={this.renderField}  
+        />
+        <Field 
+          label="Categories"
+          name="categories"
+          component={this.renderField}  
+        />
+        <Field 
+          label="Content"
+          name="content"
+          component={this.renderField}  
+        />
+      </form>
     )
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'PostNewForm'
+})(PostNew);
