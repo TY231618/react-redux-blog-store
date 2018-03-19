1d9fb2c2673d5cdeb0509f7e7c48c36963ed884d
Tony <anthony.c.young18@gmail.com>
2018-03-19 11:58:23 +0000

    rendering blog titles in PostsIndex component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class PostsIndex extends React.Component {
 
@@ -8,13 +9,36 @@ class PostsIndex extends React.Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return (
+      _.map(this.props.posts, post => {
+        return (
+          <li key={ post.id }className="list-group-item">
+            { post.title }
+          </li>
+        )
+      })
+      
+    )
+  }
+
   render() {
     return (
       <div>
-        HELLO from Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          { this.renderPosts() }
+        </ul>
       </div>
     )
   }
 }
 
-export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
